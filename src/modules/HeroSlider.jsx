3fe7a0b61fb4a187baa4d1c42a8d46c0a685f002
EURS\EX931HA <sotiris.anastasiadis@gr.ey.com>
2024-03-20 01:35:16 +0200

    minor changes

--- src/modules/HeroSlider.jsx
+++ src/modules/HeroSlider.jsx
@@ -1,35 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import {Hero} from '../components/Hero'
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
-import { Pagination } from 'swiper/modules';
-import image from '../assets/images/AppleWatch.jpg'
-import image1 from '../assets/images/AppleWatchMobile.jpg'
+import image from '../assets/images/watch.jpg'
+import imageM from '../assets/images/watch-m.jpg'
+import image1 from '../assets/images/concert.jpg'
+import image1M from '../assets/images/concert-m.jpg'
 
 export const HeroSlider = () => {
-    const slides = [
-        {
-            heroImg: image,
-            mobileImg:image1,
-            herotitle:"Discover Cutting-Edge Tech Essentials",
-            herosub: "Explore the Latest Gadgets and Innovations at Your Fingertips",
-            btntxt:"Shop now",
-        },
-        {
-            heroImg: image,
-            mobileImg:image,
-            herotitle:"Discover Cutting-Edge Tech Essentials",
-            herosub: "Explore the Latest Gadgets and Innovations at Your Fingertips",
-            btntxt:"Shop now",
-        },
-        {
-            heroImg: image,
-            mobileImg:image1,
-            herotitle:"Discover Cutting-Edge Tech Essentials",
-            herosub: "Explore the Latest Gadgets and Innovations at Your Fingertips",
-            btntxt:"Shop now",
-        },
-    ];
+  const slides = [
+    {
+        heroImg: image,
+        mobileImg: imageM,
+        herotitle: "Discover Cutting-Edge Tech Essentials",
+        herosub: "Explore the Latest Gadgets and Innovations at Your Fingertips",
+        btntxt: "Shop now",
+    },
+    {
+        heroImg: image1,
+        mobileImg: image1M,
+        herotitle: "Experience Futuristic Innovations",
+        herosub: "Embark on a Journey to Explore the Next Generation of Technology",
+        btntxt: "Discover more",
+    }
+];
+
 
 
     
@@ -40,9 +36,10 @@ export const HeroSlider = () => {
             dynamicBullets: true,
             clickable:true,
           }}
-          modules={[Pagination]}
+          modules={[Pagination, Autoplay]}
           className="mySwiper"
-          loop
+          loop={true} 
+          autoplay={{delay:5000}}
         >
             {slides.map((slide, index) => (<SwiperSlide key={index}><Hero data={slide} /></SwiperSlide>))}
       </Swiper>
