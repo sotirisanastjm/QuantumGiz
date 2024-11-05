a58d161199429fa615b373cee541a13563621699
EURS\EX931HA <sotiris.anastasiadis@gr.ey.com>
2024-11-05 21:26:03 +0200

    fix year on menu

--- src/modules/footer.jsx
+++ src/modules/footer.jsx
@@ -5,7 +5,7 @@ export const Footer= () =>{
     return (
        <footer className="px-6 py-4 bg-blue-black">
         <div className="flex flex-row justify-center">
-            <p className="font-medium">Copyright &copy; 2024 QuantumGiz </p>
+            <p className="font-medium">Copyright &copy; {new Date().getFullYear()} QuantumGiz </p>
         </div>
        </footer>
     );
